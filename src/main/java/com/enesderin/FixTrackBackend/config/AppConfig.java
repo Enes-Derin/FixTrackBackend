4878b5960b7debd4694c7ecbbe2ec28074be4607
Enes Derin <160336286+Enes-Derin@users.noreply.github.com>
2026-01-04 18:34:53 +0300

    origin config

--- src/main/java/com/enesderin/FixTrackBackend/config/AppConfig.java
+++ src/main/java/com/enesderin/FixTrackBackend/config/AppConfig.java
@@ -62,7 +62,7 @@ public class AppConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5176"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","fix-track-three.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
